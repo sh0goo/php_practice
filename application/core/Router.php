2df4fc67698ae4b241a08f65ba87990d3ffcf25d
sh0g0 <sh0g0.tksk@gmail.com>
2018-09-18 00:59:51 +0900

    ルーティング処理を実装

--- application/core/Router.php
+++ application/core/Router.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sh0g0
- * Date: 2018/02/24
- * Time: 13:42
- */
 
-/**
- * Class Router
+ /**
  * ルーティング定義配列と、PATH_INFOを受け取り、ルーティングパラメータを特定
- */
+  */
 class Router
 {
 
@@ -24,14 +17,43 @@ class Router
      * ルーティング定義配列を変換
      * @param $definitions
      */
-    public function compileRoutes($definitions)
+    public function compileRoutes(array $definitions): array
     {
-        $this->routes = array();
+        $this->routes = [];
 
-        foreach ($definitions as $url => $param){
+        foreach ($definitions as $url => $params){
+            // スラッシュごとに分割
             $tokens = explode('/', ltrim($url, '/'));
+            foreach ($tokens as $i => $token) {
+                if (0 === strpos($token, ':')) {
+                    // 分割した値の中にコロンで始まる文字列があった場合
+                    $name = substr($token, 1);
+                    $token = '(?P<'. $name . '>[^/]+)';
+                }
+                $tokens[$i] = $token;
+            }
 
+            $pattern = '/' . implode('/', $tokens);
+            $routes[$pattern] = $params;
         }
+
+        return $routes;
     }
 
+    public function resolve (string $path_info)
+    {
+        if ('/' !== substr($path_info, 0, 1)) {
+            // 先頭がスラッシュでなかった場合、スラッシュを付与
+            $path_info = '/' . $path_info;
+        }
+
+        foreach ($this->$routes as $pattern => $params) {
+            
+            if (preg_match('#^' . $pattern . '$#', $path_info, $matches)) {
+                $params = array_merge($params, $matches);
+                return $params;
+            }
+        }
+        return false;
+    }
 }
